58bd579707c99dbf866e14b9443e46c17c167cf0
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2024-01-17 12:21:46 +0100

    Add files via upload

---
+++ GROUP - OOP/GROUP - OOP/GROUP - OOP.cpp
@@ -0,0 +1,28 @@
+﻿using namespace std;
+#include "Student.h"
+#include "Date.h"
+#include "Group.h"
+
+int main()
+{
+    Student a;
+    a.AddExam(11);
+    a.AddExam(12);
+    Student b;
+    b.AddExam(10);
+    b.AddExam(9);
+    Group g;
+    g.AddStudent(a);
+    g.AddStudent(b);
+    g.Print();
+    g.Expel();
+    g.Print();
+
+    Student copya;
+    copya = a;
+    Student copyb;
+    copyb = b;
+    Group copy;
+    copy = g;
+    copy.Print();    
+}
